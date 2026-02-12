7b40da4993c248d814398fd98631e5b99caf7a1f
Joshua Wise <joshua.wise722@gmail.com>
2026-02-12 10:26:38 -0500

    Update for tailwind configs

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,9 @@
-function App() {
+export default function App() {
   return (
-    <div>
-      <h1>Hello world</h1>
+    <div className="flex h-screen w-full items-center justify-center bg-gray-900 text-white">
+      <h1 className="text-4xl font-bold underline decoration-blue-500">
+        Hello Tailwind!
+      </h1>
     </div>
   )
 }
-
-export default App;
